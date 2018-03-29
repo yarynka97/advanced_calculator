c958db7843602dab6b0a0525c389d23cc6aa8482
Yarynka Shyika <pinkpoppy.art@gmail.com>
2018-03-29 21:10:09 +0300

    Added new functonality
    fixed InputField and Button components;
    added solving method for system of linear algebraic equations 2x2 for inerval numbers;
    improved style;

--- js/main.js
+++ js/main.js
@@ -28,6 +28,7 @@
         return (
             <div className="card border-success mb-3 calc-area">
                 <h2 className="card-header">Interval Calculator</h2>
+                <p className='explanation tip'>Do simple algebraic calculations for interval numbers</p>
                 <InputArea className="card-body" />
                 <ActionChoiceAria
                     className="card-body"
@@ -51,11 +52,11 @@ var InputArea = React.createClass({
                 <p className='lead field-header'>Second interval</p>
                 <InputField
                     key="first"
-                    id="first"
+                    id="firstInterval"
                 />
                 <InputField
                     key="second"
-                    id="second"
+                    id="secondInterval"
                 />
                 <p className='tip'>Enter an interval a,b using the format as in example: 1,3.4</p>
             </div>
@@ -66,13 +67,8 @@ var InputArea = React.createClass({
 var InputField = React.createClass({
     handleTextChange: function (event) {
         var inputText = event.target.value;
-        if (inputText === '') {
-            if (this.props.id === "first") {
-                sessionStorage.removeItem('firstInterval');
-            } else {
-                sessionStorage.removeItem('secondInterval');
-            }
-        } else {
+        var name = this.props.id;
+        if (inputText) {
             var values = inputText.split(',');
             var correct = false;
             if (values.length === 2) {
@@ -87,12 +83,10 @@ var InputField = React.createClass({
             }
 
             if (correct) {
-                if (this.props.id === "first") {
-                    sessionStorage.setItem('firstInterval', values);
-                } else {
-                    sessionStorage.setItem('secondInterval', values);
-                }
+                sessionStorage.setItem(name, values);
             }
+        } else {
+            sessionStorage.removeItem(name);
         }    
     },
     render: function () {
@@ -130,8 +124,101 @@ var Button = React.createClass({
         arr.map((el, index, array) => array[index] = Number.parseFloat(el));
         return arr;
     },
-    action: function () {
-        var operation = this.props.children;;
+    add: function (firstInterval, secondInterval) {
+        var answer = [];
+        answer[0] = firstInterval[0] + secondInterval[0];
+        answer[1] = firstInterval[1] + secondInterval[1];
+        return answer;
+    },
+    subtract: function (firstInterval, secondInterval) {
+        var answer = [];
+        answer[0] = firstInterval[0] - secondInterval[1];
+        answer[1] = firstInterval[1] - secondInterval[0];
+        return answer;
+    },
+    multiplyByLambda: function (firstInterval,lambda) {
+        var answer = [];
+        if (lambda < 0) {
+            answer[0] = lambda * firstInterval[1];
+            answer[1] = lambda * firstInterval[0];
+        } else {
+            answer[0] = lambda * firstInterval[0];
+            answer[1] = lambda * firstInterval[1];
+        }
+        return answer;
+    },
+    multiply: function (firstInterval, secondInterval) {
+        var answer = [];
+        answer[0] = Math.min(firstInterval[0] * secondInterval[0], firstInterval[0] * secondInterval[1], firstInterval[1] * secondInterval[0], firstInterval[1] * secondInterval[1]);
+        answer[1] = Math.max(firstInterval[0] * secondInterval[0], firstInterval[0] * secondInterval[1], firstInterval[1] * secondInterval[0], firstInterval[1] * secondInterval[1]);
+        return answer;
+    },
+    divide: function (firstInterval, secondInterval) {
+        var answer = [];
+        answer[0] = Math.min(firstInterval[0] / secondInterval[0], firstInterval[0] / secondInterval[1], firstInterval[1] / secondInterval[0], firstInterval[1] / secondInterval[1]);
+        answer[1] = Math.max(firstInterval[0] / secondInterval[0], firstInterval[0] / secondInterval[1], firstInterval[1] / secondInterval[0], firstInterval[1] / secondInterval[1]);
+        return answer;
+    },
+    middle: function (firstInterval) {
+        var answer = (firstInterval[1] - firstInterval[0]) / 2;
+        return answer;
+    },
+    radius: function (firstInterval) {
+        var answer = (firstInterval[1] + firstInterval[0]) / 2;
+        return answer;
+    },
+    absolute: function (firstInterval) {
+        var answer = [];
+        answer[0] = 0;
+        answer[1] = Math.max(Math.abs(firstInterval[0]), Math.abs(firstInterval[1]));
+        return answer;
+    },
+    width: function (firstInterval) {
+        var answer = firstInterval[1] - firstInterval[0];
+        return answer;
+    },
+    solveMatrix: function () {
+        var answer;
+        var correct = true;
+        var A = [
+            this.getValues(sessionStorage.getItem('a11')),
+            this.getValues(sessionStorage.getItem('a12')),
+            this.getValues(sessionStorage.getItem('a21')),
+            this.getValues(sessionStorage.getItem('a22'))
+        ];
+        var B = [
+            this.getValues(sessionStorage.getItem('b1')),
+            this.getValues(sessionStorage.getItem('b2'))
+        ];
+        A.forEach(el => { if (!el) correct = false });
+        B.forEach(el => { if (!el) correct = false });
+
+        if (correct) {
+            var det = this.subtract(this.multiply(A[0], A[3]), this.multiply(A[1], A[2]));
+            if (det[0] * det[1] > 0) {
+                var A1 = [
+                    this.divide(A[3], det),
+                    this.divide(this.multiplyByLambda(A[1], -1), det),
+                    this.divide(this.multiplyByLambda(A[2], -1), det),
+                    this.divide(A[0], det)
+                ];
+
+                console.log(A1);
+                var X = [
+                    this.add(this.multiply(A1[0], B[0]), this.multiply(A1[1], B[1])),
+                    this.add(this.multiply(A1[2], B[0]), this.multiply(A1[3], B[1]))
+                ];
+                answer = X;
+            } else {
+                answer = "Change parameters, determinant has 0";
+            }
+        } else {
+            answer = "Enter all parameters";
+        };
+
+        return answer;
+    },
+    calculate: function (operation) {
         var first = sessionStorage.getItem('firstInterval');
         var second = sessionStorage.getItem('secondInterval');
         var answer = [];
@@ -142,21 +229,17 @@ var Button = React.createClass({
 
                 switch (operation) {
                     case "+":
-                        answer[0] = firstInterval[0] + secondInterval[0];
-                        answer[1] = firstInterval[1] + secondInterval[1];
+                        answer = this.add(firstInterval, secondInterval);
                         break;
                     case "-":
-                        answer[0] = firstInterval[0] - secondInterval[1];
-                        answer[1] = firstInterval[1] - secondInterval[0];
+                        answer = this.subtract(firstInterval, secondInterval);
                         break;
                     case "*":
-                        answer[0] = Math.min(firstInterval[0] * secondInterval[0], firstInterval[0] * secondInterval[1], firstInterval[1] * secondInterval[0], firstInterval[1] * secondInterval[0]);
-                        answer[1] = Math.max(firstInterval[0] * secondInterval[0], firstInterval[0] * secondInterval[1], firstInterval[1] * secondInterval[0], firstInterval[1] * secondInterval[0]);
+                        answer = this.multiply(firstInterval, secondInterval);
                         break;
                     case "/":
                         if (secondInterval[0] * secondInterval[1] > 0) {
-                            answer[0] = Math.min(firstInterval[0] / secondInterval[0], firstInterval[0] / secondInterval[1], firstInterval[1] / secondInterval[0], firstInterval[1] / secondInterval[0]);
-                            answer[1] = Math.max(firstInterval[0] / secondInterval[0], firstInterval[0] / secondInterval[1], firstInterval[1] / secondInterval[0], firstInterval[1] / secondInterval[0]);
+                            answer = this.divide(firstInterval, secondInterval);
                         } else answer[0] = 'Cannot divide this itervals, because second one contains 0';
                         break;
                     default:
@@ -166,24 +249,34 @@ var Button = React.createClass({
             } else {
                 switch (operation) {
                     case "Mid":
-                        answer = (firstInterval[1] - firstInterval[0]) / 2;
+                        answer = this.middle(firstInterval);
                         break;
                     case "Rad":
-                        answer = (firstInterval[1] + firstInterval[0]) / 2;
+                        answer = this.radius(firstInterval);
                         break;
                     case "Abs":
-                        answer[0] = 0;
-                        answer[1] = Math.max(Math.abs(firstInterval[0]), Math.abs(firstInterval[1]));
+                        answer = this.absolute(firstInterval);
                         break;
                     case "Wid":
-                        answer = firstInterval[1] - firstInterval[0];
+                        answer = this.width(firstInterval);
                         break;
                     default:
                         answer = "Enter second interval correctly";
                         break;
                 }
             }
-        }else answer = "Your input is incorrect";   
+        } else answer = "Your input is incorrect";
+
+        return answer;
+    },
+    action: function () {
+        var operation = this.props.children;
+        if (operation ==='Solve') {
+            var answer = this.solveMatrix();
+        } else {
+            var answer = this.calculate(operation);
+        }
+           
 
         sessionStorage.setItem('answer', answer);
         this.props.onAnswerFound();
@@ -195,8 +288,74 @@ var Button = React.createClass({
     }
 });
 
+var Matrix = React.createClass({
+    getInitialState: function () {
+        return {
+            answer:[]
+        }
+    },
+    answerFound: function() {
+        this.setState({ answer: sessionStorage.getItem('answer') });
+    },
+    render: function () {
+        return (
+            <div className="calc-area">
+                <h2 className="card-header">Matrix Calculator</h2>
+                <p className='explanation tip'>Solves the system of linear algebraic equations 2x2 for interval numbers</p>
+                <div className="input-area card-body">
+                    <p className='lead field-header'>a11: </p>
+                    <p className='lead field-header'>a12: </p>
+                    <InputField
+                        key="a11"
+                        id="a11"
+                    />
+                    <InputField
+                        key="a12"
+                        id="a12"
+                    />
+                    <p className='lead field-header'>a21: </p>
+                    <p className='lead field-header'>a22: </p>
+                    <InputField
+                        key="a21"
+                        id="a21"
+                    />
+                    <InputField
+                        key="a22"
+                        id="a22"
+                    />
+                    <p className='tip'>Enter interval numbers for matrix A</p>
+                    <p className='lead field-header'>b1: </p>
+                    <p className='lead field-header'>b2: </p>
+                    <InputField
+                        key="b1"
+                        id="b1"
+                    />
+                    <InputField
+                        key="b2"
+                        id="b2"
+                    />
+                    <p className='tip'>Enter interval numbers for matrix B</p>
+                </div>
+                <Button
+                    key="solveMatrix"
+                    className="btn btn-success one-step"
+                    onAnswerFound={this.answerFound}
+                >Solve</Button>
+                <div className="card-body">
+                    <h3 className="card-title">Result</h3>
+                    <p className="card-text answer"> {this.state.answer}</p>
+                </div>
+            </div>
+        )
+    }
+});
+
+
 ReactDOM.render(
-    <Calc />,
+    <div>
+        <Calc />
+        <Matrix />
+    </div>,
     document.getElementById("calc")
 );
 
